9cd06825150221efbb625b899a6418a69bbfc0e5
Omira <omiragk@outlook.com>
2026-09-11 22:49:23 +0530

    added constant arrays for doctor and ward data

--- hospital.c
+++ hospital.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <string.h>
 
+//Doctor Specialties Data
+const int specialtyID[4] = {1,2,3,4};
+const char specialtyName[4][30] = {"General Practice (OPD)","Paediatrics","Cardiology","Neurology"};
+const double baseFee[4] = {1500.00,2500.00,4500.00,5000.00};
+const int consultationTime[4] = {15,20,30,30};
+const int dailyPatientCap[4] = {30,20,12,10};
+
+//Hospital Wards Data
+const int wardID[4] = {1,2,3,4};
+const char wardName[4][30] = {"General Ward","Paediatric Ward","Surgical Ward","ICU (Intensive Care Unit)"};
+const double dailyBedRate[4] = {3000.00,6000.00,12000.00,25000.00};
+const int bedCapacity[4] = {20,10,10,5};
+
+int bedAvailabilityStatus[4][20] = {0};
